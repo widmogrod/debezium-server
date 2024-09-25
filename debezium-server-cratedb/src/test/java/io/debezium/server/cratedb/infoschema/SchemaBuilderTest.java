2c3b780c582da628bc58867d113d96dce0d4f4e7
widmogrod <widmogrod@gmail.com>
2024-09-26 00:00:05 +0200

    Add comprehensive test for SchemaBuilder

--- debezium-server-cratedb/src/test/java/io/debezium/server/cratedb/infoschema/SchemaBuilderTest.java
+++ debezium-server-cratedb/src/test/java/io/debezium/server/cratedb/infoschema/SchemaBuilderTest.java
@@ -155,4 +155,296 @@ class SchemaBuilderTest {
                 )
         ));
     }
+
+    @Test
+    void re() {
+
+        List<ColumnInfo> columns = new ArrayList<>();
+
+        ColumnInfo column1 = new ColumnInfo.Builder()
+                .setDataType("text")
+                .setColumnName("id")
+                .setIsPrimaryKey(true)
+                .setColumnDetails(new ColumnDetails("id", new ArrayList<>()))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column2 = new ColumnInfo.Builder()
+                .setDataType("object")
+                .setColumnName("doc")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<>()))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column3 = new ColumnInfo.Builder()
+                .setDataType("object")
+                .setColumnName("doc['name']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("name");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column4 = new ColumnInfo.Builder()
+                .setDataType("bigint")
+                .setColumnName("doc['name']['lucky']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("name");
+                    add("lucky");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column5 = new ColumnInfo.Builder()
+                .setDataType("boolean")
+                .setColumnName("doc['name']['truth']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("name");
+                    add("truth");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column6 = new ColumnInfo.Builder()
+                .setDataType("text")
+                .setColumnName("doc['role']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("role");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column7 = new ColumnInfo.Builder()
+                .setDataType("double precision")
+                .setColumnName("doc['#']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("#");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column8 = new ColumnInfo.Builder()
+                .setDataType("bigint")
+                .setColumnName("doc['id']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("id");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column9 = new ColumnInfo.Builder()
+                .setDataType("bigint")
+                .setColumnName("doc['name_{']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("name_{");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column10 = new ColumnInfo.Builder()
+                .setDataType("bigint")
+                .setColumnName("doc['name_object']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("name_object");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column11 = new ColumnInfo.Builder()
+                .setDataType("double precision")
+                .setColumnName("doc['-']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("-");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column12 = new ColumnInfo.Builder()
+                .setDataType("boolean_array")
+                .setColumnName("doc['name_)']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("name_)");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column13 = new ColumnInfo.Builder()
+                .setDataType("double precision")
+                .setColumnName("doc['?']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("?");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column14 = new ColumnInfo.Builder()
+                .setDataType("double precision")
+                .setColumnName("doc['<']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("<");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column15 = new ColumnInfo.Builder()
+                .setDataType("double precision")
+                .setColumnName("doc['bkt_']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("bkt_");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column16 = new ColumnInfo.Builder()
+                .setDataType("double precision")
+                .setColumnName("doc['|']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("|");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column17 = new ColumnInfo.Builder()
+                .setDataType("boolean")
+                .setColumnName("doc['timestamp without time zone']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("timestamp without time zone");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column18 = new ColumnInfo.Builder()
+                .setDataType("double precision")
+                .setColumnName("doc['!']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("!");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column19 = new ColumnInfo.Builder()
+                .setDataType("text")
+                .setColumnName("doc['name_object_text']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("name_object_text");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column20 = new ColumnInfo.Builder()
+                .setDataType("double precision")
+                .setColumnName("doc['_bkt']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("_bkt");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column21 = new ColumnInfo.Builder()
+                .setDataType("bigint")
+                .setColumnName("doc['geo_shape']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("geo_shape");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column22 = new ColumnInfo.Builder()
+                .setDataType("bigint")
+                .setColumnName("doc['name_int']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add("name_int");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        ColumnInfo column23 = new ColumnInfo.Builder()
+                .setDataType("double precision")
+                .setColumnName("doc[':']")
+                .setIsPrimaryKey(false)
+                .setColumnDetails(new ColumnDetails("doc", new ArrayList<String>() {{
+                    add(":");
+                }}))
+                .setCharacterMaximumLength(0)
+                .build();
+
+        //.. repeat this process for each log entry, creating a new ColumnInfo object each time.
+
+        columns.add(column1);
+        columns.add(column2);
+        columns.add(column3);
+        columns.add(column4);
+        columns.add(column5);
+        columns.add(column6);
+        columns.add(column7);
+        columns.add(column8);
+        columns.add(column9);
+        columns.add(column10);
+        columns.add(column11);
+        columns.add(column12);
+        columns.add(column13);
+        columns.add(column14);
+        columns.add(column15);
+        columns.add(column16);
+        columns.add(column17);
+        columns.add(column18);
+        columns.add(column19);
+        columns.add(column20);
+        columns.add(column21);
+        columns.add(column22);
+        columns.add(column23);
+
+        Schema.I expected = Schema.Dict.of(
+                "_bkt", Schema.Primitive.DOUBLE,
+                "!", Schema.Primitive.DOUBLE,
+                "role", Schema.Primitive.TEXT,
+                "#", Schema.Primitive.DOUBLE,
+                "name_object_text", Schema.Primitive.TEXT,
+                "geo_shape", Schema.Primitive.BIGINT,
+                "name_)", Schema.Array.of(Schema.Primitive.BOOLEAN),
+                "-", Schema.Primitive.DOUBLE,
+                "timestamp without time zone", Schema.Primitive.BOOLEAN,
+                "name", Schema.Coli.of(
+                        Schema.Dict.of(
+                                "lucky", Schema.Primitive.BIGINT,
+                                "truth", Schema.Primitive.BOOLEAN
+                        ),
+                        Schema.Primitive.BIGINT
+                ),
+                "name_{", Schema.Primitive.BIGINT,
+                "bkt_", Schema.Primitive.DOUBLE,
+                ":", Schema.Primitive.DOUBLE,
+                "id", Schema.Primitive.BIGINT,
+                "<", Schema.Primitive.DOUBLE,
+                "|", Schema.Primitive.DOUBLE,
+                "?", Schema.Primitive.DOUBLE
+        );
+
+        Schema.I result = SchemaBuilder.fromInformationSchema(columns);
+        assertThat(result).isEqualTo(Schema.Dict.of(
+                "id", Schema.Primitive.TEXT,
+                "doc", expected
+        ));
+    }
 }
