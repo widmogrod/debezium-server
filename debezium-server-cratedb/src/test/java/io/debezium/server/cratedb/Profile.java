d26b2ef5c08ec1bc1f3b5e43eae1aabc4a533464
widmogrod <widmogrod@gmail.com>
2024-10-10 15:06:38 +0200

    Add integration tests for MongoDB to CrateDB streaming
    Introduce `MongoCrateDBIT` class to verify data streaming from MongoDB to CrateDB. Enhance `Profile` class to include `MongoDBAndCrateDB` test profile with necessary configuration and connection setup.

--- debezium-server-cratedb/src/test/java/io/debezium/server/cratedb/Profile.java
+++ debezium-server-cratedb/src/test/java/io/debezium/server/cratedb/Profile.java
@@ -10,6 +10,9 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTestProfile;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+
+import static io.debezium.server.cratedb.Profile.MongoDBAndCrateDB.DEBEZIUM_SOURCE_MONGODB_CONNECTION_STRING;
 
 public class Profile {
 
@@ -63,4 +66,47 @@ public class Profile {
         }
     }
 
+    @EnabledIfEnvironmentVariable(named = DEBEZIUM_SOURCE_MONGODB_CONNECTION_STRING, matches = "mongo.*")
+    @QuarkusTestResource(CrateTestResourceLifecycleManager.class)
+    public static class MongoDBAndCrateDB implements QuarkusTestProfile {
+        public final static String DEBEZIUM_SOURCE_MONGODB_CONNECTION_STRING = "DEBEZIUM_SOURCE_MONGODB_CONNECTION_STRING";
+
+        @Override
+        public Map<String, String> getConfigOverrides() {
+            Map<String, String> params = new ConcurrentHashMap<>();
+
+            params.put("debezium.sink.type", "cratedb");
+            // Other params from CrateTestResourceLifecycleManager:
+            // - "debezium.sink.cratedb.connection_url"
+
+            params.put("debezium.transforms", "unwrap");
+            params.put("debezium.transforms.unwrap.type", "io.debezium.connector.mongodb.transforms.ExtractNewDocumentState");
+            params.put("debezium.transforms.unwrap.add.headers", "op");
+            params.put("debezium.transforms.unwrap.drop.tombstones", "false");
+
+            // By default, the event flattening SMT converts MongoDB arrays into arrays that are compatible with Apache Kafka Connect, or Apache Avro schemas.
+            // While MongoDB arrays can contain multiple types of elements, all elements in a Kafka array must be of the same type.
+            // FIXES: java.util.concurrent.ExecutionException: io.debezium.DebeziumException: Field name_> of schema mongo2.testdb.testcollection is not a homogenous arra
+            params.put("debezium.transforms.unwrap.array.encoding", "document");
+
+            // Other params are taken from MongoTestResourceLifecycleManager
+            // - debezium.source.mongodb.connection.string
+            params.put("debezium.source.connector.class", "io.debezium.connector.mongodb.MongoDbConnector");
+            params.put("debezium.source.topic.prefix", "my_prefix");
+            params.put("debezium.source.collection.include.list", "testdb.testcollection");
+            params.put("debezium.source.offset.storage", "org.apache.kafka.connect.storage.MemoryOffsetBackingStore");
+            params.put("debezium.source.offset.flush.interval.ms", "0");
+
+            params.put("debezium.source.capture.mode", "change_streams_update_full");
+            params.put("debezium.source.capture.mode.full.update.type", "lookup");
+
+            // CrateDB sink can infer schema on it's own
+            // no need to send schema information
+            params.put("debezium.format.schemas.enable", "false");
+            params.put("debezium.format.header.schemas.enable", "false");
+            params.put("debezium.format.value.schemas.enable", "false");
+
+            return params;
+        }
+    }
 }
