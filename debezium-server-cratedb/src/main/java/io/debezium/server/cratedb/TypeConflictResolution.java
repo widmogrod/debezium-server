b3ef97aabbb8cd2c73add0e8be810a01716fe0cf
widmogrod <widmogrod@gmail.com>
2024-10-17 17:18:22 +0200

    Add TYPE_WRAP_AND_MALFORMED strategy to TypeConflictResolution
    Added a new strategy called TYPE_WRAP_AND_MALFORMED for dealing with type conflicts. Updated the stringify and parse methods to handle the new strategy. This provides additional flexibility in conflict resolution scenarios.

--- debezium-server-cratedb/src/main/java/io/debezium/server/cratedb/TypeConflictResolution.java
+++ debezium-server-cratedb/src/main/java/io/debezium/server/cratedb/TypeConflictResolution.java
@@ -9,11 +9,13 @@ public class TypeConflictResolution {
     final public static String NAME_SILENT_NULL = "null";
     final public static String NAME_STORE_MALFORMED_FRAGMENTS = "malformed";
     final public static String NAME_TYPE_SUFFIX_AND_MALFORMED = "type_suffix_and_malformed";
+    final public static String NAME_TYPE_WRAP_AND_MALFORMED = "type_wrap_and_malformed";
 
     public enum Strategy {
         SILENT_NULL,
         STORE_MALFORMED_FRAGMENTS,
-        TYPE_SUFFIX_AND_MALFORMED;
+        TYPE_SUFFIX_AND_MALFORMED,
+        TYPE_WRAP_AND_MALFORMED;
     }
 
     public static String stringify(Strategy strategies) {
@@ -21,6 +23,7 @@ public class TypeConflictResolution {
             case SILENT_NULL -> NAME_SILENT_NULL;
             case STORE_MALFORMED_FRAGMENTS -> NAME_STORE_MALFORMED_FRAGMENTS;
             case TYPE_SUFFIX_AND_MALFORMED -> NAME_TYPE_SUFFIX_AND_MALFORMED;
+            case TYPE_WRAP_AND_MALFORMED -> NAME_TYPE_WRAP_AND_MALFORMED;
         };
     }
 
@@ -28,6 +31,7 @@ public class TypeConflictResolution {
         return switch (strategyName.trim()) {
             case NAME_SILENT_NULL -> Strategy.SILENT_NULL;
             case NAME_TYPE_SUFFIX_AND_MALFORMED -> Strategy.TYPE_SUFFIX_AND_MALFORMED;
+            case NAME_TYPE_WRAP_AND_MALFORMED -> Strategy.TYPE_WRAP_AND_MALFORMED;
             default -> Strategy.STORE_MALFORMED_FRAGMENTS;
         };
     }
